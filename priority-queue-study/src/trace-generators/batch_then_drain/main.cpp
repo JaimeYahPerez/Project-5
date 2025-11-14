5dcd428571fdbaf42ed644ef9134778dc77b2874
Jaime <jaime.yahperez@gmail.com>
2025-11-13 22:50:35 -0800

    initial push

---
+++ priority-queue-study/src/trace-generators/batch_then_drain/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+
+#include "../../../utils/TraceConfig.hpp"
+#include<random>
+#include<iomanip>
+
+
+void generateTrace(const unsigned seed,
+    const std::size_t n,
+    TraceConfig& config,
+    std::uniform_int_distribution<int>& dist,
+    std::mt19937& gen) {
+
+    // create and open the output file name
+    auto outputFileName = config.makeTraceFileName(seed, n);
+    std::cout << "File name: " << outputFileName << std::endl;
+    std::ofstream out(outputFileName.c_str());
+    if (!out.is_open()) {
+        std::cerr << "Failed to open file " << outputFileName << std::endl;
+        exit(1);
+    }
+    out << config.profileName << " " << n << " " << seed << std::endl;
+
+    // Generate N inserts.
+    for (unsigned id = 0; id < n; ++id) {
+        out << "I " << "          " << dist(gen)
+            << "          " << id << "\n";
+    }
+
+    // Generate N extract-mins (drain)
+    for (unsigned i = 0; i < n; ++i) {
+        out << "E\n";
+    }
+    out.close();
+}
+
+
+int main() {
+
+    // TraceConfig provides pre-configured values such as N and seed
+    TraceConfig config(std::string("batch_then_drain"));
+    for (auto seed : config.seeds) {  // currently, we are using one seed only.
+        std::mt19937 rng(seed);   // create a random-number generator using "seed"
+
+        for (auto n : config.Ns) {
+            // We need to limit the range of values that get generated.
+            // To model the Huffman tree behavior, we want to generate a lot of
+            // duplicates. So, we use choose_key_upper_bound to increase
+            // the upperbound according to the size of the trace.
+            const int key_min = 1;
+            const int key_max = static_cast<int>(8 * n); // or an even larger fixed bound
+            std::uniform_int_distribution<int> dist(key_min, key_max);
+
+            generateTrace(seed, n, config, dist, rng);
+        }
+
+    }
+
+    return 0;
+}
